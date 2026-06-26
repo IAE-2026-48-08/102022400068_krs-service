1dc7c937f44a426938cc5096675194e71b9f738b
binjal1410 <feelinlove2005@gmail.com>
2026-06-26 14:05:27 +0700

    chore: configure database to use SQLite, track .env, and add global exception handling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -22,4 +22,24 @@ return Application::configure(basePath: dirname(__DIR__))
         $exceptions->shouldRenderJsonWhen(
             fn (Request $request) => $request->is('api/*'),
         );
+
+        $exceptions->render(function (\Symfony\Component\HttpKernel\Exception\NotFoundHttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'Not Found',
+                    'errors' => (object)[]
+                ], 404);
+            }
+        });
+
+        $exceptions->render(function (\Illuminate\Database\Eloquent\ModelNotFoundException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'Not Found',
+                    'errors' => (object)[]
+                ], 404);
+            }
+        });
     })->create();
